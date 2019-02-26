e213a608e3334102eb7ca05f22e64c3a3335fbad
Bohdan <bohdan.martyniuk1488@gmail.com>
2019-02-26 15:05:45 -0500

    all tasks done expect for 3-module 2-task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,41 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+function find(obj, value){
+
+    
+    let arrIsObj = [];
+    let notObj;
+        
+
+    for ( let key in obj ) {
+        
+        let newVal = obj[key]
+        if( typeof newVal === 'object' ) {
+
+            let _find;
+
+            if( _find = find( newVal, value ) )
+            arrIsObj.push( key + '.' + _find);
+
+            //console.log(_find, arrValue);
+
+        } else {
+
+            if( newVal === value ) {
+                notObj = key;
+            }
+
+        }
+
+    }
+
+    if( arrIsObj.length > 0 ){
+        return ( arrIsObj.length === 1 ) ? arrIsObj.join(',') : arrIsObj;
+    } else if(notObj !== undefined) {
+        return notObj;
+    } else {
+        return null
+    }
+
 }
