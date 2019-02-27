b6ef7760e12127b9f75c234d70c2c7e276fb2485
Bohdan <bohdan.martyniuk1488@gmail.com>
2019-02-26 21:56:01 -0500

    all tasks solved and submitted again

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,39 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
-};
+  
+    let begin = this.from;
+    let finish = this.to;
 
+    let fromDate = begin.getDate() + 1;
+    let toDate = finish.getDate() + 1;
+
+    let countMonth = ( new Date(begin.getFullYear(), begin.getMonth(), 0).getDate());
+
+  
+    return {
+        next() {
+            if (begin.setDate(fromDate) <= finish.setDate(toDate)) {
+                if (fromDate > countMonth) {
+                    fromDate = finish.getDate() - 1;
+                }
+                
+
+               let putBrackets = (begin.getDay() === 5 || begin.getDay() === 6);
+               
+                return {
+                    done: false,
+                    value: fromDate < 10 ? putBrackets ? '[' +  0 + '' + fromDate++ + ']' : 
+                     0 + '' + fromDate++   : putBrackets ? '[' +  '' + fromDate++ + ']':
+                     '' + fromDate++ 
+                }
+            } else {
+                return {
+                    done: true
+                }
+            }
+        }
+    }
+    
+}
 
