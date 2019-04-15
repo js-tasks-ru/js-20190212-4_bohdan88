30a6b1671717a321aeacbb745c90c37ec8254014
Bohdan <bohdan.martyniuk1488@gmail.com>
2019-04-14 23:27:18 -0400

    task 11 done

--- 11-module/1-task/index.js
+++ 11-module/1-task/index.js
@@ -37,12 +37,42 @@
          * @param {Element} root - элемент, внутри которого, нужно включить работу подсказок
          */
         attach(root) {
-        }
+            document.onmouseover = (e) => {
+                let target = e.target
+                let tooltip = target.getAttribute('data-tooltip')
+                if (!tooltip) return;
 
+                this.el.classList.add('tooltip_active');
+                this.el.innerHTML = tooltip;
+
+                let coord = target.getBoundingClientRect();
+                const heightDoc = document.documentElement.clientHeight;
+
+                let left = coord.left + (target.offsetWidth - this.el.offsetWidth) / 2;
+                if (left < 0) left = 8;
+
+                let top = coord.top + target.offsetHeight + this.indent;
+                if (heightDoc - top < this.el.offsetHeight) {
+                    top = coord.top - (this.el.offsetHeight + this.indent);
+                }
+
+                this.el.style.left = left + 'px';
+                this.el.style.top = top + 'px';
+            }
+            document.onmouseout = () => {
+
+                    this.el.classList.remove('tooltip_active')
+
+
+            }
+        }
         /**
          * Удаляет все обработчики событий
          */
         detach() {
+            document.onmouseover = e => {
+                e.preventDefault();
+            }
         }
     }
 
